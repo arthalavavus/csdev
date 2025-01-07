f53c41c926a2d1d108e0b295812d3a47c0b7ab5f
Baptiste Thomassin <baptiste.thomassin@cpe.fr>
2025-01-07 18:18:35 +0100

    voila

--- tlw/magic-lotr/script.js
+++ tlw/magic-lotr/script.js
@@ -3,14 +3,19 @@ function parseMana(str) {
 }
 async function afficherCartes()
 {
+    
     let contenu_json = [];
-    fetch('https://api.scryfall.com/cards/search?q=e:ltr&format=json&order=set&unique=prints%20lang:fr')
-    .then(response=> response.json())
-    .then(donnee => {
-        for (carte of donnee.data)  {
-            contenu_json.push(carte.printed_name);
-        }
-        console.log(contenu_json);
+
+        fetch('https://api.scryfall.com/cards/search?q=e:ltr lang:fr&format=json&order=set&unique=prints%20')
+        .then(response=> response.json())
+        .then(donnee => {
+            for (carte of donnee.data)  { 
+                contenu_json.push(carte.printed_name);
+                contenu_json.push(carte.image_uris);
+        
+            }
+    
+            console.log(contenu_json);
     });
 }
 
